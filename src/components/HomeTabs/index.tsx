7f96c93c11d4343e57f20d383ad8b8c40b360c0e
tbreslein <tommy.breslein@protonmail.com>
2022-09-12 22:19:07 +0200

    styling: add icons to bottom HomeTabs

--- src/components/HomeTabs/index.tsx
+++ src/components/HomeTabs/index.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import ToDosScreen from '../../screens/ToDosScreen';
 import DeadlinesScreen from '../../screens/DeadlinesScreen';
 import HouseholdScreen from '../../screens/HouseHoldScreen';
@@ -7,13 +8,44 @@ import UserProfileScreen from '../../screens/UserProfileScreen';
 
 const Tab = createMaterialBottomTabNavigator();
 
+// NOTE:: check https://reactnavigation.org/docs/material-bottom-tab-navigator#using-with-react-native-paper-optional
+// for styling examples
+
 const HomeTabs = () => {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="ToDos" component={ToDosScreen} />
-      <Tab.Screen name="Deadlines" component={DeadlinesScreen} />
-      <Tab.Screen name="Household" component={HouseholdScreen} />
-      <Tab.Screen name="UserProfile" component={UserProfileScreen} />
+      <Tab.Screen
+        name="ToDos"
+        component={ToDosScreen}
+        options={{
+          tabBarLabel: 'ToDos',
+          tabBarIcon: () => <MaterialCommunityIcons name="format-list-checks" size={26} />,
+        }}
+      />
+      <Tab.Screen
+        name="Deadlines"
+        component={DeadlinesScreen}
+        options={{
+          tabBarLabel: 'Deadlines',
+          tabBarIcon: () => <MaterialCommunityIcons name="alarm" size={26} />,
+        }}
+      />
+      <Tab.Screen
+        name="Household"
+        component={HouseholdScreen}
+        options={{
+          tabBarLabel: 'Household',
+          tabBarIcon: () => <MaterialCommunityIcons name="home-variant-outline" size={26} />,
+        }}
+      />
+      <Tab.Screen
+        name="UserProfile"
+        component={UserProfileScreen}
+        options={{
+          tabBarLabel: 'Profile',
+          tabBarIcon: () => <MaterialCommunityIcons name="account" size={26} />,
+        }}
+      />
     </Tab.Navigator>
   );
 };
